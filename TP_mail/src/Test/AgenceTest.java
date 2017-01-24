a6cab006f8508515576d93e0f64481425de02b7f
dauphineMail <lenovo@DESKTOP-B08T47Q>
2017-01-24 23:25:24 +0100

    test

--- TP_mail/src/Test/AgenceTest.java
+++ TP_mail/src/Test/AgenceTest.java
@@ -3,20 +3,28 @@ package Test;
 import junit.framework.TestCase;
 import BanqueP.*;
 public class AgenceTest extends TestCase {
-	public void testAjouterClient() throws Exception{
+	public void testAjouterClient() {
 		Agence agence=new Agence(02,"Agence A","Adresse",null);
-		Client client=new Client (25,100,agence);
 		agence.ajouterClient(25);
 		assertEquals(1, agence.getListeClient().size());
 		
 	}
-	public void SupprimerClient() throws Exception{
+	
+	public void supprimerClient() {
 		Agence agence=new Agence(02,"Agence A","Adresse",null);
 		Client client=new Client (25,100,agence);
 		agence.getListeClient().add(client);
 		//assertEquals(true, agence.getListeClient().contains(client));
 		agence.supprimerClient(client);
 		assertEquals(false, agence.getListeClient().contains(client));
+		
+	}
+	public void créditerClient() {
+		Agence agence=new Agence(02,"Agence A","Adresse",null);
+		Client client=new Client (25,100,agence);
+		agence.getListeClient().add(client);
+		agence.crediterCompte(client, 25.3);
+		assertEquals(125.3, agence.getListeClient().get(0).getSolde());
 		
 	}
 
